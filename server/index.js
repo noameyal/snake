9083edadd1443260ccdc42d5f2bdcde4dcdffa61
noameyal <noameyal1@gmail.com>
2019-11-26 05:00:44 +0000

    Set up client

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(express.static("client"));
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`)
 })
